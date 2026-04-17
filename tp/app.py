417c6d44d1047f506e340e9c52d74bfe2f2f0486
hhryan135-dot <hhryan135@gmail.com>
2026-04-17 21:14:59 +0200

    Update app.py

--- tp/app.py
+++ tp/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import math
+import pandas as pd
 
 diff_x = st.number_input("Scostamento in x", format="%.3f")
 diff_y = st.number_input("Scostamento in y", format="%.3f")
@@ -7,3 +8,11 @@ diff_y = st.number_input("Scostamento in y", format="%.3f")
 if st.button("Calcola"):
     result = round((2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2)))), 3)
     st.success(result)
+    
+data = {
+"Scostamento in x" : diff_x,
+"Scostamento in y" : diff_y,
+"True Position" : result
+}
+
+st.dataframe(df)
