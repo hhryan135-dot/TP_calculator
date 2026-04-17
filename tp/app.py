22124d1dde57f0b7a49f873a00b43b86a9bd3f78
hhryan135-dot <hhryan135@gmail.com>
2026-04-17 20:43:37 +0200

    Update app.py

--- tp/app.py
+++ tp/app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import math
 
-diff_x = st.number_input("Scostamento in x")
-diff_y = st.number_input("Scostamento in y")
+diff_x = st.number_input("Scostamento in x", format="%.3f")
+diff_y = st.number_input("Scostamento in y", format="%.3f")
 
 if st.button("Calcola"):
-    result = (2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2))))
+    result = round((2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2)))), 3)
+    st.success(result)
