04d689a38a06bf2709144ccc6a940537e1d5c763
hhryan135-dot <hhryan135@gmail.com>
2026-04-17 21:18:34 +0200

    Update app.py

--- tp/app.py
+++ tp/app.py
@@ -8,11 +8,12 @@ diff_y = st.number_input("Scostamento in y", format="%.3f")
 if st.button("Calcola"):
     result = round((2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2)))), 3)
     st.success(result)
-    
-data = {
-"Scostamento in x" : diff_x,
-"Scostamento in y" : diff_y,
-"True Position" : result
-}
+    data = {
+    "Scostamento in x" : diff_x,
+    "Scostamento in y" : diff_y,
+    "True Position" : result
+    }
+    df = pd.DataFrame(data)
+
 
 st.dataframe(df)
