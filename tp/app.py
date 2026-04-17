4b94ebe749acf25ad2a063a2e255fc9efb23b827
hhryan135-dot <hhryan135@gmail.com>
2026-04-17 21:33:00 +0200

    Update app.py

--- tp/app.py
+++ tp/app.py
@@ -11,7 +11,7 @@ if st.button("Calcola"):
     result = round((2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2)))), 3)
     st.success(result)
     st.session_state.history.append({
-        "Scostamento in x": diff_x),
+        "Scostamento in x": diff_x,
         "Scostamento in y": diff_y,
         "True Position": result
     })
