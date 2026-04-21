2e8885cead7037805ee7f35c3fd4f2e7df80401c
hhryan135-dot <hhryan135@gmail.com>
2026-04-21 20:49:44 +0200

    Update app.py

--- tp/app.py
+++ tp/app.py
@@ -14,10 +14,13 @@ if st.button("Calcola"):
     else:
         result = round((2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2)))), 3)
         st.success(result)
+        out_tol = result - tp_tol if result <= tp_tol else 0
         st.session_state.history.append({
             "Scostamento in x": diff_x,
             "Scostamento in y": diff_y,
-            "Esatta Posizione": result
+            "Esatta Posizione": result,
+            "Tolleranza di posizione": tp_tol,
+            "Fuori tolleranza": out_tol
         })
 
 df = pd.DataFrame(st.session_state.history)
